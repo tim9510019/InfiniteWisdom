241db90069b8bb32c8bba4b4c4bb547b6980d187
TimGenius <tim9510019@hotmail.com>
2024-06-01 14:07:36 +0800

    Website V1.0.0 240601

--- src/components/Home/CategorySection.tsx
+++ src/components/Home/CategorySection.tsx
@@ -1,8 +1,20 @@
 import { type ClassifiedPosts } from "@/types/home";
-import { useState } from "react";
+import { useState, type ChangeEvent } from "react";
 import CardSm from "./CardSm";
-import NextPrevBtn from "./NextPrevBtn";
 import { Separator } from "../ui/separator";
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardContent,
+} from "../ui/card";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+} from "../ui/pagination";
+import { Button } from "../ui/button";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 type PostIdToMinutesMap = {
   [postId: string]: number;
@@ -19,7 +31,9 @@ const CategorySection = ({
   classifiedPosts,
   postIdToMinutesMap,
 }: CategorySectionProps) => {
-  const posts = classifiedPosts[category];
+  const posts = classifiedPosts[category].sort(
+    (a, b) => b.data.date.getTime() - a.data.date.getTime()
+  );
   const [page, setPage] = useState<number>(1);
   const postsPerPage = 3;
   const pages = Math.ceil(posts.length / postsPerPage);
@@ -27,20 +41,63 @@ const CategorySection = ({
   for (let i = 0; i < posts.length; i += postsPerPage) {
     groupedPosts.push(posts.slice(i, i + postsPerPage));
   }
+  const currentPosts = groupedPosts[page - 1];
+  const handleNextPage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setPage((prev) => {
+      return Math.min(prev + 1, pages);
+    });
+  };
+  const handlePrevPage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setPage((prev) => Math.max(prev - 1, 1));
+  };
 
   return (
-    <section className="flex flex-col gap-4 overflow-hidden">
-      <a href={`/categories/${category}`}>
-        <h2 className="text-xl font-semibold hover:underline">{category}</h2>
-      </a>
-      <Separator />
-      <div className="relative grid grid-cols-3 gap-3 group">
-        {/* <NextPrevBtn position="left" />
-        <NextPrevBtn position="right" /> */}
-        {classifiedPosts[category].slice(0,3).reverse().map((post, index) => (
+    <Card className="flex flex-col overflow-hidden">
+      <CardHeader className="flex flex-row items-center justify-between max-sm:py-4">
+        <div>
+          <CardTitle>
+            <a href={`/categories/${category}`} className="w-fit">
+              <h2 className="text-2xl font-bold">{category}</h2>
+            </a>
+          </CardTitle>
+          <CardDescription>共有{posts.length}篇貼文</CardDescription>
+        </div>
+        <div>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <Button
+                  size="icon"
+                  variant="outline"
+                  className="h-9 w-9"
+                  onClick={handlePrevPage}
+                >
+                  <FaAngleLeft />
+                </Button>
+              </PaginationItem>
+              <div className="text-sm">
+                {page} / {pages}
+              </div>
+              <PaginationItem>
+                <Button
+                  size="icon"
+                  variant="outline"
+                  className="h-9 w-9"
+                  onClick={handleNextPage}
+                >
+                  <FaAngleRight />
+                </Button>
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
+      </CardHeader>
+      <CardContent className="relative flex flex-col gap-4 group max-sm:gap-2">
+        {currentPosts.map((post, index) => (
           <CardSm
             key={index}
             url={`/blog/${post.slug}`}
+            category={post.data.category}
             subcategory={post.data.subcategory}
             title={post.data.title}
             imageSrc={post.data.heroImage.src.src}
@@ -48,10 +105,11 @@ const CategorySection = ({
             date={post.data.date}
             author={post.data.author}
             minutes={postIdToMinutesMap[post.id]}
+            body={post.body}
           />
         ))}
-      </div>
-    </section>
+      </CardContent>
+    </Card>
   );
 };
 
