b49c9154506664f96fbb6b4b442f0a218e3e36a6
TimGenius <tim9510019@hotmail.com>
2024-06-03 08:42:12 +0800

    Website V1.0.0 240603

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -14,33 +14,33 @@ type SocialLink = {
 };
 
 export const socialLinks: SocialLink[] = [
-  {
-    platform: "facebook",
-    url: "https://www.facebook.com/profile.php?id=100003234932687",
-    icon: <FaFacebook size={25} />,
-    iconAlt: "facebook",
-  },
-  {
-    platform: "instagram",
-    url: "https://www.instagram.com/john_lifetraveler/",
-    icon: <FaInstagram size={25} />,
-    iconAlt: "instagram",
-  },
+  // {
+  //   platform: "facebook",
+  //   url: "https://www.facebook.com/profile.php?id=100003234932687",
+  //   icon: <FaFacebook size={25} />,
+  //   iconAlt: "facebook",
+  // },
+  // {
+  //   platform: "instagram",
+  //   url: "https://www.instagram.com/john_lifetraveler/",
+  //   icon: <FaInstagram size={25} />,
+  //   iconAlt: "instagram",
+  // },
   {
     platform: "github",
-    url: "https://github.com/johnnoob",
+    url: "https://github.com/tim9510019",
     icon: <FaGithub size={25} />,
     iconAlt: "github",
   },
   {
     platform: "twitter",
-    url: "https://github.com/johnnoob",
+    url: "https://x.com/chenzhengting1",
     icon: <FaXTwitter size={25} />,
     iconAlt: "twitter",
   },
   {
     platform: "linkedIn",
-    url: "https://github.com/johnnoob",
+    url: "https://www.linkedin.com/in/jeng-ting-chen-b05658123/",
     icon: <FaLinkedin size={25} />,
     iconAlt: "linkedIn",
   },
